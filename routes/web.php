e20562e81eaa31b03e817bc03fee534538416324
koirherianto <koir.herianto@gmail.com>
2023-12-03 17:39:26 +0800

    landing page

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use App\Http\Controllers\UserController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing.index');
 });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
